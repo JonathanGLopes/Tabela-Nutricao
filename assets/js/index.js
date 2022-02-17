eed5786cf5a91e99243d92a114a0183616a960ff
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-02-17 18:17:44 -0300

    Validação peso e altura

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,6 +11,27 @@ let altura = tdAltura.textContent;
 
 let tdImc = paciente.querySelector(".info-imc");
 
-let imc = peso / (altura * altura);
+let conferePeso = true;
+let confereAltura = true;
 
-tdImc.textContent = imc;
+/* 
+    Verificação nas informações adicionadas.
+    As variaveis acima iniciam como true, porém se os dados não baterem com a 
+    verificação ela vai mudar para false e exibir a mensagem de erro
+*/
+
+if (peso <= 0 || peso >= 500) {
+    conferePeso = false;
+    tdImc.textContent = "Peso inválido";
+}
+
+if (altura <= 0 || altura >= 3.00){
+    confereAltura = false;
+    tdAltura.textContent = "Altura inválida";
+    tdImc.textContent = "IMC inválido";
+}
+
+if (confereAltura && conferePeso){
+    let imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
