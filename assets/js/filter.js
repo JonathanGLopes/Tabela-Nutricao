e79179a5272de61900e009d225c2391bdfa92266
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-03-29 20:02:04 -0300

    Implementando a lógica de filtragem

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,10 +1,24 @@
 const $filtro = document.querySelector("#filtro-tabela");
 
-$filtro.addEventListener("input", () =>{
+$filtro.addEventListener("input", function(){
     let pacientes = document.querySelectorAll(".paciente");
-    for(let i = 0; i < pacientes.length; i++){
-        let paciente = pacientes[i];
-        let tdNome = paciente.querySelector(".info-nome");
-        let nome = paciente.textContent;
+
+    if( this.value.length > 0){
+        for(let i = 0; i < pacientes.length; i++){
+            let paciente = pacientes[i];
+            let tdNome = paciente.querySelector(".info-nome");
+            let nome = tdNome.textContent;
+    
+            if(nome != this.value){
+                paciente.classList.add("invisivel");
+            } else{
+                paciente.classList.remove("invisivel");
+            }
+        }
+    } else {
+        for (let i = 0; i < pacientes.length; i++){
+            let paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
     }
 });
